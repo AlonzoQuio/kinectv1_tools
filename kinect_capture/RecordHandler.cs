7f214d9eedd79f2c4e491d3aea5440dd5c4e0639
Alonzo <alonzo.q@gmail.com>
2018-07-09 15:24:57 -0500

    Modification of DepthBasic example to save rgb and depth images continuously

---
+++ kinect_capture/RecordHandler.cs
@@ -0,0 +1,40 @@
+﻿using System.Threading;
+
+namespace Microsoft.Samples.Kinect.DepthBasics
+{
+    class RecordHandler
+    {
+        RecordThread record=null;
+        public RecordHandler(RecordThread record)
+        {
+            this.record = record;
+        }
+        public void Start()
+        {
+            if (record != null)
+            {
+                Thread thread = new Thread(record.DoWork);
+                thread.Start();
+            }
+
+        }
+        public bool Close()
+        {
+            bool status = false;
+            if (record != null)
+            {
+                record.RequestStop();
+                status = true;
+            }
+            return status;
+        }
+        public void EnableRecord()
+        {
+            record.EnableRecord();
+        }
+        public void DisableRecord()
+        {
+            record.DisableRecord();
+        }
+    }
+}
